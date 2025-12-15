7b7c9c7a3e868e462e281a61025152718f05746d
Mohamad Fikri <146539030+mofasa798@users.noreply.github.com>
2025-12-15 21:00:12 +0700

    feat: add blog post listing and single post view

--- routes/web.php
+++ routes/web.php
@@ -28,8 +28,55 @@ Route::get('/about', function () {
     ]);
 });
 
+
+
 Route::get('/blog', function () {
     return view('posts', [
-        'title' => 'Posts'
+        $blog_post = [
+    [
+        "title" => "Judul Post Pertama",
+        "slug" => "judul-post-pertama",
+        "author" => "Ujang",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus explicabo, atque nemo autem unde perferendis culpa magnam odio ex fugit, soluta sit qui distinctio consequuntur natus voluptate aperiam praesentium error!"
+    ],
+    [
+        "title" => "Judul Post Kedua",
+        "slug" => "judul-post-kedua",
+        "author" => "Dudung",
+        "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, vero illo accusamus excepturi necessitatibus perferendis fugiat quis itaque perspiciatis et expedita nihil doloremque similique unde id, aspernatur dolor aut velit quibusdam? Eum, corrupti?"
+    ]
+    ],
+        'title' => 'Posts',
+        'posts' => $blog_post
+    ]);
+});
+
+// halaman single posts
+Route::get('/posts/{slug}', function ($slug) {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Ujang",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus explicabo, atque nemo autem unde perferendis culpa magnam odio ex fugit, soluta sit qui distinctio consequuntur natus voluptate aperiam praesentium error!"
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Dudung",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis, vero illo accusamus excepturi necessitatibus perferendis fugiat quis itaque perspiciatis et expedita nihil doloremque similique unde id, aspernatur dolor aut velit quibusdam? Eum, corrupti?"
+        ]
+    ];
+
+    $new_post = [];
+    foreach ($blog_post as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $new_post
     ]);
 });
